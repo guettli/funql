a48cd0393203afc71b54e81ac6bcf701f49343a3
Thomas Guettler <guettliml@thomas-guettler.de>
2021-12-21 22:03:10 +0100

    symlink packages fetched via `npm install`.

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static-root'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
